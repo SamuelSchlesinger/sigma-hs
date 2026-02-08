8db3979ceca6ca9869f8e31194e385a4e6cc0dcf
Samuel Schlesinger <sgschlesinger@gmail.com>
2026-02-08 13:46:34 -0500

    fixed build

--- Setup.hs
+++ Setup.hs
@@ -1,12 +1,8 @@
 import Distribution.Simple
-import Distribution.Simple.LocalBuildInfo (LocalBuildInfo, localPkgDescr, withLibLBI, componentBuildDir)
-import Distribution.Types.BuildInfo (BuildInfo, extraLibDirs)
-import Distribution.Types.Library (Library, libBuildInfo)
-import Distribution.Types.PackageDescription (PackageDescription, library)
+import Distribution.Simple.LocalBuildInfo (localPkgDescr, withLibLBI, componentBuildDir)
 import System.Process (callProcess)
 import System.Directory (getCurrentDirectory, copyFile, createDirectoryIfMissing)
 import System.FilePath ((</>))
-import Data.IORef
 
 main :: IO ()
 main = defaultMainWithHooks simpleUserHooks
@@ -17,24 +13,11 @@ main = defaultMainWithHooks simpleUserHooks
       callProcess "cargo" ["build", "--release", "--manifest-path", manifestPath]
       lbi <- confHook simpleUserHooks (gpd, hbi) flags
       let rustLibDir = cwd </> "rust" </> "sigma-ffi" </> "target" </> "release"
-      -- Copy the native library into the component build directory so it
-      -- gets installed alongside the Haskell library and is available to
-      -- downstream packages.
-      buildDirRef <- newIORef Nothing
+      -- Copy the native library into the component build directory so
+      -- extra-bundled-libraries can find it.
       withLibLBI (localPkgDescr lbi) lbi $ \_ clbi -> do
-        let bdir = componentBuildDir lbi clbi
+        let bdir = cwd </> componentBuildDir lbi clbi
         createDirectoryIfMissing True bdir
         copyFile (rustLibDir </> "libsigma_ffi.a") (bdir </> "libsigma_ffi.a")
-        writeIORef buildDirRef (Just bdir)
-      mBuildDir <- readIORef buildDirRef
-      -- Update extra-lib-dirs to use the component build directory
-      let pd = localPkgDescr lbi
-      case (mBuildDir, library pd) of
-        (Just bdir, Just lib) -> do
-          let bi = libBuildInfo lib
-              bi' = bi { extraLibDirs = bdir : filter (/= "rust/sigma-ffi/target/release") (extraLibDirs bi) }
-              lib' = lib { libBuildInfo = bi' }
-              pd' = pd { library = Just lib' }
-          return lbi { localPkgDescr = pd' }
-        _ -> return lbi
+      return lbi
   }
